ea206c8e9db1fad9e4bf86709c438ffc068096c9
Alfred Chang <gnahc1992@gmail.com>
2017-06-27 04:58:05 +0000

    Added code to enable user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  validates :username, presence: true, length: {minimum: 4, maximum: 16}
+  validates :username, presence: true, length: {minimum: 4, maximum: 30}
 end
