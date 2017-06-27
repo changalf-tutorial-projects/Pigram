4ee27c26cda5c343086871ab4c967517ff89fe05
Alfred Chang <gnahc1992@gmail.com>
2017-06-27 03:38:03 +0000

    Added username column to table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627030038) do
+ActiveRecord::Schema.define(version: 20170627033221) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
@@ -36,9 +36,11 @@ ActiveRecord::Schema.define(version: 20170627030038) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
