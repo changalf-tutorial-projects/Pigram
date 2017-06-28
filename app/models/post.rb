6d2c5d1de7a73dc3377978b92017817feb4b7ebe
Alfred Chang <gnahc1992@gmail.com>
2017-06-28 05:50:34 +0000

    Added time posted for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   validates :user_id, presence: true
   validates :image, presence: true
+  validates :caption, length: {minimum: 1, maximum: 300}, allow_blank: false
   
   belongs_to :user
   has_many :comments, dependent: :destroy
