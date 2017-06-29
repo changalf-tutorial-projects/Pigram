1d0275a06e54bf3e035fdd8c32a4dc54f89f9505
Alfred Chang <gnahc1992@gmail.com>
2017-06-29 06:09:25 +0000

    Added feature to display either posted image or placeholder image when editing post

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -3,3 +3,8 @@ $(document).on('turbolinks:load', function() {
     $('.alert').slideUp(500);
   }, 1000);
 })
+
+var loadFile = function(event) {
+  var output = document.getElementById('image-preview');
+  output.src = URL.createObjectURL(event.target.files[0]);
+};
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
      notice: 'alert-info'}
      [flash_type.to_sym] || flash_type.to_s
   end
+  
+  def form_image_select(post) 
+    if post.image.exists?
+      return image_tag post.image.url(:medium), id: 'image-preview', class: 'img-responsive'
+    else
+      # Placeholder image from http://www.futurespast.co/wp-content/themes/bh_courage/assets/images/placeholder_featured_image.svg 
+      return image_tag '/assets/placeholder-img.svg', id: 'image-preview', class: 'img-responsive'
+    end
+  end
 end
