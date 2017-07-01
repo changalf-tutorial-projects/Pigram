0e475eb296c05ddce0917482ea66869a33d8ccda
Alfred Chang <gnahc1992@gmail.com>
2017-07-01 04:00:04 +0000

    Added user instance variable

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   def show
+    @user = User.find_by(username: params[:username])
     @posts = User.find_by(username: params[:username]).posts.order('created_at DESC')
   end
 end
