06f7455d53fdb8ed6132cc7440f04da31c9ff753
Alfred Chang <gnahc1992@gmail.com>
2017-07-02 07:12:21 +0000

    Display number of likes if at least 8 likes

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,12 +1,10 @@
 module PostsHelper
-  def likers_of(post)
+  def display_likes(post) 
     votes = post.votes_for.up.by_type(User)
-    usernames = []
-    unless votes.blank?
-      votes.voters.each do |voter|
-        usernames.push(link_to voter.username, profile_path(voter.username), class: 'user-name')
-      end
-      usernames.to_sentence.html_safe + like_plural(votes)
+    if votes.size <= 8
+      return list_likers(votes) 
+    else 
+      count_likers(votes)
     end
   end
   
@@ -19,6 +17,21 @@ module PostsHelper
   end
   
   private
+    def list_likers(votes)
+      usernames = []
+      unless votes.blank?
+        votes.voters.each do |voter|
+          usernames.push(link_to voter.username, profile_path(voter.username), class: 'user-name')
+        end
+        usernames.to_sentence.html_safe + like_plural(votes)
+      end
+    end
+    
+    def count_likers(votes)
+      vote_count = votes.size
+      vote_count.to_s + ' likes'
+    end
+  
     def like_plural(votes)
       if votes.count >= 2
         return ' like this' 
