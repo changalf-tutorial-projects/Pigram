2a8615dcf6672f73f0b3673c418ae9140f66b323
Alfred Chang <gnahc1992@gmail.com>
2017-07-02 10:35:58 +0000

    Added validations to notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,6 @@
 class Notification < ActiveRecord::Base
+  validates :user_id, :notified_by_id, :post_id, :identifier, :notice_type, presence: true
+  
   belongs_to :user
   # :subscribed_user = :notified_by
   belongs_to :subscribed_user, class_name: 'User'
