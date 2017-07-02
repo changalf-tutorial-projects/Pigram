dde11177cecbb9fe006bcb698b85c89b065b698f
Alfred Chang <gnahc1992@gmail.com>
2017-07-02 02:26:46 +0000

    Added like action to before_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :like, :destroy]
   before_action :authenticate_user!
   before_action :owned_post, only: [:edit, :update, :destroy]
   
