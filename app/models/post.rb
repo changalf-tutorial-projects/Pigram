19bd71d3683b007cc44230f703734081fddfd19c
Alfred Chang <gnahc1992@gmail.com>
2017-07-02 04:08:23 +0000

    Changed post dimensions; layout changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,6 @@ class Post < ActiveRecord::Base
   validates :image, presence: true
   validates :caption, length: {minimum: 1, maximum: 300}, allow_blank: false
   
-  has_attached_file :image, styles: {:medium => "640x"}
+  has_attached_file :image, styles: {medium: "1000x500"}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
