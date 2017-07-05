9076a69bf9f77ccdc8031111a12e5f5cd499c8d5
Alfred Chang <gnahc1992@gmail.com>
2017-07-05 05:43:07 +0000

    Fixed missing colon

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
   has_many :followers, through: :follower_relationships, source: :follower
   
-  has_many :following_relationships, foreign_key: follower_id, class_name: 'Follow'
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
   has_many :following, through: :following_relationships, source: :following
          
   validates :username, presence: true, length: {minimum: 4, maximum: 30}
